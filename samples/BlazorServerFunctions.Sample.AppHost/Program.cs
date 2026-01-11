07e828c0a413db90f416eb9875ed39dfc7ab7476
ncigula <nikola.cigula@vanado.hr>
2026-01-11 16:21:35 +0100

    Add aspire xunit v3 integration test project

--- samples/BlazorServerFunctions.Sample.AppHost/Program.cs
+++ samples/BlazorServerFunctions.Sample.AppHost/Program.cs
@@ -2,6 +2,6 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<BlazorServerFunctions_Sample>("webApp");
+builder.AddProject<BlazorServerFunctions_Sample>("sample");
 
 await builder.Build().RunAsync();
